94cb3c8f4809c13ff0030a14676ea24044a1c494
Yung Choi <neochoi@neochoi2.seo.corp.google.com>
2017-03-12 00:21:42 +0900

    Fix test_predict

--- test_predict.py
+++ test_predict.py
@@ -2,7 +2,7 @@
 # It works for test.csv and produces human readable output.
 #
 # Usage: python test_predict.py <model_dir> [kifu_feature_csv]
-# Sample usage: python test_predict.py 0310 small.csv
+# Sample usage: python test_predict.py tr0310 small.csv
 
 import numpy as np
 import tensorflow as tf
@@ -10,11 +10,11 @@ import tensorflow as tf
 import sys
 
 if len(sys.argv) < 2:
-  pirnt('Usage: python test_predict.py <model_dir> [kifu_feature_csv]')
+  print('Usage: python test_predict.py <model_dir> [kifu_feature_csv]')
   exit(1)
-model_dir = sys.argv[2]
-test_set = sys.argv[3]
-print('Test %d file with model in %s' % (test_set, model_dir)
+model_dir = sys.argv[1]
+test_set = sys.argv[2]
+print('Test %s file with model in %s' % (test_set, model_dir))
 
 # Load and define features
 test_set = tf.contrib.learn.datasets.base.load_csv_without_header(
@@ -24,28 +24,30 @@ feature_columns = [tf.contrib.layers.real_valued_column("", dimension=84)]
 
 # Load model and predict
 classifier = tf.contrib.learn.DNNClassifier(
-    model_dir=model_dir, config=config,
+    model_dir=model_dir,
     feature_columns=feature_columns, hidden_units=[20, 20], n_classes=3)
 ds_predict_tf  = classifier.predict(x_test) 
 
 # Print out human readable.
-BOARD_CHAR = { -1: 'O', 1: 'X', 0: '+' }
-NEXT_TURN_MSG = { -1: 'BLACK(X)', 1: 'WHITE(O)' }
+BOARD_CHAR = { -1: 'O', 1: '@', 0: '.' }
+NEXT_TURN_MSG = { -1: 'BLACK(@)', 1: 'WHITE(O)' }
 RESULT_MSG = { 0: 'WHITE', 1: 'JIGO', 2: 'BLACK' }
-for i in range(len(ds_predict_tf)):
-  feature = x_test[i]
+idx = 0
+for pred in ds_predict_tf:
+  feature = x_test[idx]
   board = feature[:81]
   last_move = feature[81]
   ko_pos = feature[82:84]
-  idx = 0
-  for row in range(1:10):
+  pos = 0
+  for row in range(1, 10):
     outstr = ''
-    for col in range(1:10):
-      if row == ko_pos[0] and col == ko_pos:
+    for col in range(1, 10):
+      if row == ko_pos[0] and col == ko_pos[1]:
         outstr = outstr + '*'
       else:
-        outstr = outstr + BOARD_CHAR[board[idx]]
-      idx = idx + 1
+        outstr = outstr + BOARD_CHAR[board[pos]]
+      pos = pos + 1
     print outstr
-  print('%s turn, predict: %s, real: %s' %
-        (NEXT_TURN_MSG[last_move], RESULT_MSG[ds_predict_tf[i]], RESULT_MSG[y_test[i]]))
+  print('%s turn, predict: %s, real: %s\n' %
+        (NEXT_TURN_MSG[last_move], RESULT_MSG[pred], RESULT_MSG[y_test[idx]]))
+  idx = idx + 1
